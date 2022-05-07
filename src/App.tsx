d0fee5ad5eef27c8042760ce361656e8c4b51f1b
jaret-queercoded <97352155+jaret-queercoded@users.noreply.github.com>
2022-05-07 15:21:07 -0400

    Start of a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navigation/Navbar';
 function App() {
   return (
-    <div className="App">
-      Hello, my name is Jaret Screws and I am queer_coded. This website is a work in progress.
+    <div>
+      <Navbar />
+      <div className="App">
+        Hello, my name is Jaret Screws and I am queer_coded. This website is a work in progress.
+      </div>
     </div>
   );
 }
