73f1d2a63c7bf730a0819afa49106934d260ec7b
Jaret Screws <jaret@queercoded.io>
2022-05-06 20:38:44 -0400

    Place Holder Stuff

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello, my name is Jaret Screws and I am queer_coded. This website is a work in progress.
     </div>
   );
 }
