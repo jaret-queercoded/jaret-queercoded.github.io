888615011df63ed985381899ef43e5c5f47c3fe8
jaret-queercoded <97352155+jaret-queercoded@users.noreply.github.com>
2022-06-05 13:40:15 -0400

    Some padding on list items

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,12 @@
 const Projects = () => {
     return(
+        // this still is not right but it is much better
         <div className="Projects" dir="rtl">
             <h1>Projects</h1>
             <ul>
                 <li><a href="https://github.com/jaret-queercoded/quee">quee</a></li>
                 <ul>
-                    <li>quee is a 2D game engine that is written in C using SDL2 and has Lua integration!</li>
+                    <li>quee is a 2D game engine that is written in C using SDL2 and utilizes Lua integration</li>
                     <li>Development is done mostly live on <a href="https://twitch.tv/queer_coded">Twitch</a></li>
                     <li>Stream archives can be found on <a href="https://www.youtube.com/channel/UCkN5wBtmX-nGKa_FMQ-7r_w">YouTube</a></li>
                 </ul>
@@ -17,7 +18,7 @@ const Projects = () => {
                 </ul>
                 <li><a href="https://github.com/jaret-queercoded/doto">doto</a></li>
                 <ul>
-                    <li>doto is a ncurses based terminal application for tracking codebase todo priority!</li>
+                    <li>doto is a ncurses based terminal application for tracking codebase todo priority</li>
                     <li>By simply tacking more o's onto the end of your todo it will raise the priority on your todo list</li>
                     <li>doto will recursively search your entire code base for all todos and display them in an user friendly list</li>
                 </ul>
